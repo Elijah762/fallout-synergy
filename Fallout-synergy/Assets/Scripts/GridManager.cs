14ff6b5d831eaa8e1116700148f705c5b1639869
Elijah <stryker1850@gmail.com>
2023-03-01 19:32:38 -0600

    Adding grid generation

---
+++ Fallout-synergy/Assets/Scripts/GridManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+	[SerializedField]
+	private int width;
+	private int height;
+	
+	[SerializedField]
+	private Tile tilePrefab;
+
+    [SerializedField]
+    private Transform cam;
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+        generateGrid();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	
+	void generateGrid() {
+		for(int x = 0; x < width; x++) {
+			for(int y = 0; y < width; y++) {
+                var spawnedTile = Instantiate(tilePrefab, new Vector3(x, y), Quaternian.identity);
+                spawnedTile.name = $"Tile {x} {y}";
+            }
+		}
+        cam.transform.position = new Vector3((float)width/2 - .5f, float)height/2 - .5f, -10)
+	}
+}
+
