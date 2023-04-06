cf40ec1f2b19633a0e3e28b6a4774a7e0e559c07
Elijah <stryker1850@gmail.com>
2023-04-06 13:21:56 -0500

    Added multi tile generation (Mountain + Ground)

--- Fallout-synergy/Assets/Scripts/GridManager.cs
+++ Fallout-synergy/Assets/Scripts/GridManager.cs
@@ -2,13 +2,14 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
- 
+using Random = UnityEngine.Random;
+
 public class GridManager : MonoBehaviour {
-    [SerializeField] private int _width, _height;
+    [SerializeField] private int width, height;
  
-    [SerializeField] private Tile _tilePrefab;
+    [SerializeField] private Tile grassTile, mountainTile;
  
-    [SerializeField] private Transform _cam;
+    [SerializeField] private Transform cam;
  
     private Dictionary<Vector2, Tile> _tiles;
  
@@ -18,20 +19,20 @@ public class GridManager : MonoBehaviour {
  
     void GenerateGrid() {
         _tiles = new Dictionary<Vector2, Tile>();
-        for (int x = 0; x < _width; x++) {
-            for (int y = 0; y < _height; y++) {
-                var spawnedTile = Instantiate(_tilePrefab, new Vector3(x, y), Quaternion.identity);
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                var randomTile = Random.Range(0, 6) == 3 ? mountainTile : grassTile;
+                var spawnedTile = Instantiate(randomTile, new Vector3(x, y), Quaternion.identity);
                 spawnedTile.name = $"Tile {x} {y}";
- 
-                var isOffset = (x % 2 == 0 && y % 2 != 0) || (x % 2 != 0 && y % 2 == 0);
-                spawnedTile.Init(isOffset);
+                
+                spawnedTile.Init(x, y);
  
  
                 _tiles[new Vector2(x, y)] = spawnedTile;
             }
         }
  
-        _cam.transform.position = new Vector3((float)_width/2 -0.5f, (float)_height / 2 - 0.5f,-10);
+        cam.transform.position = new Vector3((float)width/2 -0.5f, (float)height / 2 - 0.5f,-10);
     }
  
     public Tile GetTileAtPosition(Vector2 pos) {
