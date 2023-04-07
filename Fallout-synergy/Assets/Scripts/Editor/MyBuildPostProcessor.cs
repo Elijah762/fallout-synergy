3cb2d94f945f630383a1e9f9943fb4182330e7fd
Elijah <stryker1850@gmail.com>
2023-04-07 00:11:57 -0500

    Added Executable

---
+++ Fallout-synergy/Assets/Scripts/Editor/MyBuildPostProcessor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.IO;
+using UnityEditor;
+using UnityEditor.Callbacks;
+
+public class MyBuildPostProcessor
+{
+    [PostProcessBuildAttribute(1)]
+    public static void OnPostprocessBuild(BuildTarget target, string pathToBuiltProject)
+    {
+        string path = Path.GetDirectoryName(pathToBuiltProject) + "\\data";
+        if(Directory.Exists(path)) Directory.Delete(path, true);
+        
+        FileUtil.CopyFileOrDirectory("data", path);
+    }
+}
