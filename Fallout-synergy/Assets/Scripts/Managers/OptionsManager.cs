24d7c26f499a7750f78be60ffc8424addcb5b793
Elijah <stryker1850@gmail.com>
2023-04-12 15:36:49 -0500

    Added resolution updating

--- Fallout-synergy/Assets/Scripts/Managers/OptionsManager.cs
+++ Fallout-synergy/Assets/Scripts/Managers/OptionsManager.cs
@@ -2,10 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class OptionsManager : MonoBehaviour
 {
     public Toggle fullscreenTog, vsyncTog;
+
+    public List<ResItem> resolutions = new List<ResItem>();
+
+    private int selectedResolution;
+
+    public TMP_Text resolutionLabel;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,10 +27,36 @@ public class OptionsManager : MonoBehaviour
         
     }
 
+    public void ResLeft()
+    {
+        selectedResolution--;
+        if (selectedResolution < 0) {selectedResolution = 0;}
+        
+        UpdateResLabel();
+    }
+
+    public void ResRight()
+    {
+        selectedResolution++;
+        if (selectedResolution > resolutions.Count - 1) {selectedResolution = resolutions.Count - 1;}
+        UpdateResLabel();
+    }
+
+    public void UpdateResLabel()
+    {
+        resolutionLabel.text = resolutions[selectedResolution].horizontal.ToString() + " x " + resolutions[selectedResolution].vertical.ToString();
+    }
+    
     public void ApplyGraphics()
     {
-        Screen.fullScreen = fullscreenTog.isOn ;
-
         QualitySettings.vSyncCount = vsyncTog ? 1 : 0;
+        
+        Screen.SetResolution(resolutions[selectedResolution].horizontal, resolutions[selectedResolution].vertical, fullscreenTog.isOn);
     }
+}
+
+[System.Serializable]
+public class ResItem
+{
+    public int horizontal, vertical;
 }
